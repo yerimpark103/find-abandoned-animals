e145ba19bb7920f106f50e5b985a7889611ed3ba
claud_park <claudia@relate.so>
2023-04-05 12:33:07 +0900

    Add favicon and index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        {/* Show list view page as default page */}
         <ListViewPage />
       </main>
     </>
