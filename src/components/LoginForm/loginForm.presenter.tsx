e6209d717030d021b04dfa7a189f70c2adab029d
dragonfruitlemonade <tpghks9245@gmail.com>
2023-04-09 20:30:17 +0900

    로그인폼 이름 변경

--- src/components/LoginForm/loginForm.presenter.tsx
+++ src/components/LoginForm/loginForm.presenter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Input, Button, InputProps} from "antd";
+import {Input, Button} from "antd";
 import { Wrapper, FormWrapper, ButtonWrapper } from "./loginForm.styles";
 
 // interface LoginProps {
